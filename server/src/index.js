87e976f001b3052b53c6a2e45c6954c3a875acf8
Miguel Angel <miguelangeldev@icloud.com>
2022-10-03 23:17:24 -0400

    feat: add schemas and apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
-//TODO
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+// Mocks
+const mocks = require('./mocks');
+
+// Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  mocks,
+});
+
+// Server listening
+server.listen().then(() => {
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at http://localhost:4000
+  `);
+})
